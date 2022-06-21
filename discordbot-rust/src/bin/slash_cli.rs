7db31a037ac33bb659689213dc1adace6512c329
gradiuscypher <gradiuscypher@gmail.com>
2022-06-21 14:45:44 -0700

    working cli stuff

--- discordbot-rust/src/bin/slash_cli.rs
+++ discordbot-rust/src/bin/slash_cli.rs
@@ -1,6 +1,6 @@
 use clap::{Parser, Subcommand};
+use discordbot_rust::commands::app_commands::install_commands;
 use log::{error, info};
-use serenity::http::{self};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -12,31 +12,25 @@ struct Args {
 #[derive(Subcommand, Debug)]
 enum Commands {
     Install {
+        #[clap(value_parser)]
         token: String,
+        #[clap(value_parser)]
         app_id: u64,
+        #[clap(value_parser)]
         guild_id: u64,
     },
 }
 
 async fn install_to_guild(token: &str, app_id: u64, guild_id: u64) {
-    let client = http::Http::new_with_application_id(&token, app_id);
-    let target_guild = client.get_guild(guild_id).await;
-
-    match target_guild {
-        Ok(guild) => {
-            // guild.set_application_commands(http, |commands| {
-            //     commands.create_application_command(|command| {
-            //         command.name()
-            //     })
-            // })
-        }
-        Err(e) => error!("Unabled to fetch guild: {}", e),
+    match install_commands(token, app_id, guild_id).await {
+        Ok(commands) => info!("Created commands: {:?}", commands),
+        Err(e) => error!("{e}"),
     }
 }
 
 #[tokio::main]
 async fn main() {
-    log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
     let args = Args::parse();
 
     match args.command {
--- discordbot-rust/src/commands/app_commands.rs
+++ discordbot-rust/src/commands/app_commands.rs
@@ -1,18 +1,31 @@
+use anyhow::{anyhow, Error, Result};
 use log::{error, info};
 use serenity::http::{self};
+use serenity::model::interactions::application_command::ApplicationCommand;
+use serenity::{builder::CreateApplicationCommand, model::id::GuildId};
 
-async fn install_to_guild(token: &str, app_id: u64, guild_id: u64) {
+pub async fn install_commands(
+    token: &str,
+    app_id: u64,
+    guild_id: u64,
+) -> Result<Vec<ApplicationCommand>, Error> {
+    let application_commands: Vec<CreateApplicationCommand> = Vec::new();
     let client = http::Http::new_with_application_id(&token, app_id);
-    let target_guild = client.get_guild(guild_id).await;
+    // let target_guild = client.get_guild(guild_id).await.unwrap();
+    let target_guild = GuildId(guild_id);
 
-    match target_guild {
-        Ok(guild) => guild.set_application_commands(client, |commands| {
+    let commands = target_guild
+        .set_application_commands(client, |commands| {
             commands.create_application_command(|command| {
                 command
                     .name("echo_modal")
-                    .description("Creates an example echo modal.")
+                    .description("Creates an echo modal example")
             })
-        }),
-        Err(e) => error!("Unabled to fetch guild: {}", e),
+        })
+        .await;
+
+    match commands {
+        Ok(commands) => Ok(commands),
+        Err(e) => Err(anyhow!("Unable to create commands: {e}")),
     }
 }
