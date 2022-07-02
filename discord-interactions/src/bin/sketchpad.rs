e101cadbaf3fa4e2ff23108d1eda022ad2bfb031
gradiuscypher <gradiuscypher@gmail.com>
2022-07-01 19:24:09 -0700

    more random persist work

---
+++ discord-interactions/src/bin/sketchpad.rs
@@ -0,0 +1,5 @@
+use discord_interactions::commands::hackerone::persistence::local;
+
+fn main() {
+    local::last_ticket_number(None);
+}
--- discord-interactions/src/commands/hackerone/persistence/local.rs
+++ discord-interactions/src/commands/hackerone/persistence/local.rs
@@ -1,15 +1,40 @@
-pub struct Persistence {
-    last_ticket_number: Option<String>,
+use serde::Deserialize;
+use std::path::Path;
+use std::{fs::File, io::ErrorKind};
+
+#[derive(Debug, Deserialize)]
+struct Persistence {
+    last_ticket_number: String,
 }
 
-impl Persistence {
-    pub fn last_ticket_number(&mut self, ticket_number: Option<String>) -> Option<String> {
-        match ticket_number {
-            Some(ticket_number) => {
-                self.last_ticket_number = Some(ticket_number);
-                self.last_ticket_number.clone()
+pub fn last_ticket_number(ticket_number: Option<String>) -> Option<String> {
+    let file_path = Path::new("local_persistence.json");
+    let file = File::open(file_path);
+
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::options()
+                .write(true)
+                .read(true)
+                .create(true)
+                .open(file_path)
+            {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
             }
-            None => self.last_ticket_number.clone(),
-        }
-    }
+        },
+    };
+
+    let persistence: Persistence = serde_json::from_reader(file).expect("error while reading");
+    println!("{:?}", persistence);
+    Some("bob".to_owned())
+
+    // match ticket_number {
+    //     Some(ticket_number) => Some("bob".to_owned()),
+    //     None => None,
+    // }
 }
