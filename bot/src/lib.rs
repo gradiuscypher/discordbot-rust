113fd939c7358e1297dc23ed34204d30c179a876
gradiuscypher <gradiuscypher@gmail.com>
2023-01-10 21:51:16 -0800

    working on bot version using poise

---
+++ bot/src/lib.rs
@@ -0,0 +1,30 @@
+// ref: https://github.com/serenity-rs/poise/blob/develop/examples/framework_usage/main.rs
+
+pub mod commands;
+use std::{collections::HashMap, sync::Mutex};
+
+// Types used by all command functions
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+// Custom user data passed to all command functions
+pub struct Data {
+    pub votes: Mutex<HashMap<String, u32>>,
+}
+
+pub async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
+    // This is our custom error handler
+    // They are many errors that can occur, so we only handle the ones we want to customize
+    // and forward the rest to the default handler
+    match error {
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Command { error, ctx } => {
+            println!("Error in command `{}`: {:?}", ctx.command().name, error,);
+        }
+        error => {
+            if let Err(e) = poise::builtins::on_error(error).await {
+                println!("Error while handling error: {}", e)
+            }
+        }
+    }
+}
